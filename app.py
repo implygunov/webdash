5f1c398c92f88733f5c5d3775c2249930a922cca
implygunov <noudovman@gmail.com>
2026-09-07 15:09:01 -0300

    Fix PYEOF syntax error at end of app.py

--- app.py
+++ app.py
@@ -413,4 +413,3 @@ if __name__ == '__main__':
     init_db()
     is_debug = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1")
     app.run(host='0.0.0.0', port=5000, debug=is_debug)
-PYEOF
